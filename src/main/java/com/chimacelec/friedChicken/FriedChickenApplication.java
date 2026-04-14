b9f79360d431c392d5102e2263d8e4333038abc2
reistrem <reistrem@gmail.com>
2026-04-14 21:20:16 +0900

    First Commit&Push

---
+++ src/main/java/com/chimacelec/friedChicken/FriedChickenApplication.java
@@ -0,0 +1,13 @@
+package com.chimacelec.friedChicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriedChickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriedChickenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chimacelec/friedChicken/FriedChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimacelec.friedChicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriedChickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
